1289a1ba6aad83fe4e666124f89c95b4660427e0
chenwenzhuo <Chenwz1996@163.com>
2018-10-20 00:19:42 +0800

    滑动窗口法解决“无重复字符最长子串长度”问题

---
+++ LongestSubstring_NoRepeatingChars/src/com/heythere/Main.java
@@ -0,0 +1,11 @@
+package com.heythere;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solutions solutions = new Solutions();
+        System.out.println(solutions.lengthOfLongestSubstring("abcabcbb"));
+
+        System.out.println(solutions.lengthOfLongestSubstring("pwwkew"));
+    }
+}
