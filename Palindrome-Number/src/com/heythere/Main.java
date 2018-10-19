008394d1eb9f8c0cd0724aff99e17c6ac2a9cba5
chenwenzhuo <Chenwz1996@163.com>
2018-10-19 17:14:36 +0800

    Palindrome-Number完成了以下算法：转换为字符串、反转数字、反转一半数字

--- Palindrome-Number/src/com/heythere/Main.java
+++ Palindrome-Number/src/com/heythere/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Solutions solutions = new Solutions();
-        System.out.println(solutions.isPalindrome(0));
+        System.out.println(solutions.isPalindrome_JudgeByString(1234321));
+        System.out.println(solutions.isPalindrome_JudgeByReverseHalf(123321));
     }
 }
