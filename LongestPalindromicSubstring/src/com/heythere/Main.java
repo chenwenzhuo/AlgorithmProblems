5d47a032f1a458a478e25487837de3c987b72d4b
chenwenzhuo <Chenwz1996@163.com>
2018-10-22 23:55:26 +0800

    LongestPalindromicSubstring完成了添加字符到HashMap的操作，下一步检查回文

--- LongestPalindromicSubstring/src/com/heythere/Main.java
+++ LongestPalindromicSubstring/src/com/heythere/Main.java
@@ -3,6 +3,7 @@ package com.heythere;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String str = "abcdefg";
+        System.out.println(str.substring(0, 2));
     }
 }
--- LongestPalindromicSubstring/src/com/heythere/Solutions.java
+++ LongestPalindromicSubstring/src/com/heythere/Solutions.java
@@ -1,21 +1,38 @@
 package com.heythere;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class Solutions {
     public String longestPalindrome(String s) {
-        List<CharOccurrence> charsInString=new ArrayList<>();
-        HashMap<Character,Integer> charsAndOccurrence=new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            if(!charsAndOccurrence.containsKey(s.charAt(i))){
-                charsAndOccurrence.put(s.charAt(i),1);
-            }else{
-
-            }
+        //检查参数是否合法
+        if (null == s || s.equals("")) {
+            throw new IllegalArgumentException("参数非法，字符串为空或null");
         }
 
-        return "";
+        //用HashMap保存s中所有字符及字符出现次数
+        HashMap<Character, Integer> chars_Occurrences = new HashMap<>();
+        //最长回文子串
+        String longestPalindromicSubstring = null;
+
+        for (int i = 0; i < s.length(); i++) {
+            char aCharOfStr = s.charAt(i);
+
+            if (!chars_Occurrences.containsKey(aCharOfStr)) {
+                //遇到新字符，将其加入HashMap中
+                chars_Occurrences.put(s.charAt(i), 1);
+                continue;
+            }
+
+            //遇到HashMap中已有的字符，更新它的出现次数
+            int oldOccurrence = chars_Occurrences.get(aCharOfStr);
+            chars_Occurrences.replace(aCharOfStr, oldOccurrence, oldOccurrence + 1);
+
+            /*
+            下一步检查：
+            更新了occurrence的字符与前方此字符的occurrence构成的子串是否回文
+             */
+        }
+
+        return longestPalindromicSubstring;
     }
 }
