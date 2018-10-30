89174b4bdeaf78b1c9799afb32665cc30a993984
chenwenzhuo <Chenwz1996@163.com>
2018-10-30 15:58:30 +0800

    有bug，如何控制Z字拐弯？

---
+++ ZigzagConversion/src/com/heythere/Main.java
@@ -0,0 +1,13 @@
+package com.heythere;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*char[][] chars = new char[3][3];
+        System.out.println(chars[1][1]);
+        String str = "abc";
+        System.out.println(str.contains("" + chars[1][1]));*/
+        Solutions solutions = new Solutions();
+        System.out.println(solutions.convert("PAYPALISHIRING", 3));
+    }
+}
