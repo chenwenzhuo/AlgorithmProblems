4fc4f9f751142e4c177ec2af015e0371a4ed2a49
chenwenzhuo <Chenwz1996@163.com>
2018-11-05 20:45:34 +0800

    获取子串时没有考虑到越界问题

--- RomanToInteger/src/com/heythere/Main.java
+++ RomanToInteger/src/com/heythere/Main.java
@@ -1,11 +1,9 @@
 package com.heythere;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
-        List<String> strings = new ArrayList<>();
+        Solutions solutions = new Solutions();
+        System.out.println(solutions.romanToInt("MCMXCIV"));
     }
 }
--- RomanToInteger/src/com/heythere/Solutions.java
+++ RomanToInteger/src/com/heythere/Solutions.java
@@ -1,29 +1,128 @@
 package com.heythere;
 
+import java.util.HashMap;
+
 public class Solutions {
+    private HashMap<String, Integer> thousandDigits;
+    private HashMap<String, Integer> hundredDigits;
+    private HashMap<String, Integer> tenDigits;
+    private HashMap<String, Integer> singleDigits;
+
+    private int romanStringIndex = 0;
+
     public int romanToInt(String s) {
+        addThousandKV();
+        addHundredKV();
+        addTenKV();
+        addSingleKV();
 
+        int integerFromRoman = 0;
+        integerFromRoman = integerFromRoman + getThousandDigit(s) * 1000;
+        integerFromRoman = integerFromRoman + getHundredDigit(s) * 100;
+        integerFromRoman = integerFromRoman + getTenDigit(s) * 10;
+        integerFromRoman = integerFromRoman + getSingleDigit(s);
 
+        return integerFromRoman;
+    }
+
+    private void addThousandKV() {
+        thousandDigits = new HashMap<>();
+
+        thousandDigits.put("M", 1);
+        thousandDigits.put("MM", 2);
+        thousandDigits.put("MMM", 3);
+    }
+
+    private void addHundredKV() {
+        hundredDigits = new HashMap<>();
+
+        hundredDigits.put("C", 1);
+        hundredDigits.put("CC", 2);
+        hundredDigits.put("CCC", 3);
+        hundredDigits.put("CD", 4);
+        hundredDigits.put("D", 5);
+        hundredDigits.put("DC", 6);
+        hundredDigits.put("DCC", 7);
+        hundredDigits.put("DCCC", 8);
+        hundredDigits.put("CM", 9);
+    }
+
+    private void addTenKV() {
+        tenDigits = new HashMap<>();
+
+        tenDigits.put("X", 1);
+        tenDigits.put("XX", 2);
+        tenDigits.put("XXX", 3);
+        tenDigits.put("XL", 4);
+        tenDigits.put("L", 5);
+        tenDigits.put("LX", 6);
+        tenDigits.put("LXX", 7);
+        tenDigits.put("LXXX", 8);
+        tenDigits.put("XC", 9);
+    }
+
+    private void addSingleKV() {
+        singleDigits = new HashMap<>();
+
+        singleDigits.put("I", 1);
+        singleDigits.put("II", 2);
+        singleDigits.put("III", 3);
+        singleDigits.put("IV", 4);
+        singleDigits.put("V", 5);
+        singleDigits.put("VI", 6);
+        singleDigits.put("VII", 7);
+        singleDigits.put("VIII", 8);
+        singleDigits.put("IX", 9);
+    }
+
+    private int getThousandDigit(String s) {
+        String thousandDigitSubstring;
+
+        for (int i = 3; i > 0; i--) {
+            thousandDigitSubstring = s.substring(romanStringIndex, romanStringIndex + i);
+            if (thousandDigits.containsKey(thousandDigitSubstring)) {
+                romanStringIndex += thousandDigitSubstring.length();
+                return thousandDigits.get(thousandDigitSubstring);
+            }
+        }
         return 0;
     }
 
-    private int executeThousand(String s, int startAt) {
-        String thousand = s.substring(startAt, startAt + 3);
+    private int getHundredDigit(String s) {
+        String hundredDigitSubstring;
 
-        if (thousand.equals("MMM")) {
-            return 3;
-        } else {
-            thousand = s.substring(startAt, startAt + 2);
+        for (int i = 4; i > 0; i--) {
+            hundredDigitSubstring = s.substring(romanStringIndex, romanStringIndex + i);
+            if (hundredDigits.containsKey(hundredDigitSubstring)) {
+                romanStringIndex += hundredDigitSubstring.length();
+                return hundredDigits.get(hundredDigitSubstring);
+            }
         }
+        return 0;
+    }
 
-        if (thousand.equals("MM")) {
-            return 2;
-        } else {
-            thousand = s.substring(startAt, startAt + 1);
+    private int getTenDigit(String s) {
+        String tenDigitSubstring;
+
+        for (int i = 4; i > 0; i--) {
+            tenDigitSubstring = s.substring(romanStringIndex, romanStringIndex + i);
+            if (tenDigits.containsKey(tenDigitSubstring)) {
+                romanStringIndex += tenDigitSubstring.length();
+                return tenDigits.get(tenDigitSubstring);
+            }
         }
+        return 0;
+    }
 
-        if (thousand.equals("M")) {
-            return 1;
+    private int getSingleDigit(String s) {
+        String singleDigitSubstring;
+
+        for (int i = 4; i > 0; i--) {
+            singleDigitSubstring = s.substring(romanStringIndex, romanStringIndex + i);
+            if (singleDigits.containsKey(singleDigitSubstring)) {
+                romanStringIndex += singleDigitSubstring.length();
+                return singleDigits.get(singleDigitSubstring);
+            }
         }
         return 0;
     }
