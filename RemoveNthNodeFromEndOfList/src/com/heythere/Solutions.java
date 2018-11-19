d22b1bdf802ffc218d9bae3e83fc1752fe64f5f9
chenwenzhuo <Chenwz1996@163.com>
2018-11-19 16:25:43 +0800

    created project RemoveNthNodeFromEndOfList

---
+++ RemoveNthNodeFromEndOfList/src/com/heythere/Solutions.java
@@ -0,0 +1,10 @@
+package com.heythere;
+
+public class Solutions {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode[] lastNodesFromEnd = new ListNode[n + 1];
+
+
+        return head;
+    }
+}
