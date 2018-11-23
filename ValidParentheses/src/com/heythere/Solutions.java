0eede07fed4c0f3991a7af700dec4b810f9b3dbe
chenwenzhuo <Chenwz1996@163.com>
2018-11-23 15:35:38 +0800

    start ValidParentheses

---
+++ ValidParentheses/src/com/heythere/Solutions.java
@@ -0,0 +1,14 @@
+package com.heythere;
+
+public class Solutions {
+    public boolean isValid(String s) {
+        //长度为奇数，一定不匹配
+        if (1 == s.length() % 2) {
+            return false;
+        }
+        
+
+
+        return false;
+    }
+}
