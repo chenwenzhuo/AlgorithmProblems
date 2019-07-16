96a7b5b8ae6c6aff5431c1cf2b400ee0fd9c00d9
chenwenzhuo <Chenwz1996@163.com>
2019-07-16 21:59:13 +0800

    以O(n^2)的复杂度解决了ThreeSum问题

---
+++ ThreeSum/src/com/hey_there/Main.java
@@ -0,0 +1,13 @@
+package com.hey_there;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 0, 1, 2, -1, -4};
+
+        Solutions solutions = new Solutions();
+        System.out.println(solutions.threeSum(nums));
+    }
+}
