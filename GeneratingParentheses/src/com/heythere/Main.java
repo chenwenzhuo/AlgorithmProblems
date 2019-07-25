a114ea9d15ed3e89f3d6f4b810446bde2ee2f863
chenwenzhuo <Chenwz1996@163.com>
2019-07-25 22:21:57 +0800

    使用试错的算法跑通了，但效率低。最优算法直接生成所有组合

--- GeneratingParentheses/src/com/heythere/Main.java
+++ GeneratingParentheses/src/com/heythere/Main.java
@@ -1,8 +1,22 @@
 package com.heythere;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner input = new Scanner(System.in);
+        System.out.print("请输入您想生成多少对括号的组合：");
+        int parenthesesPairs = input.nextInt();
+
+        BestSolution bestSolution = new BestSolution();
+        List<String> combinations = bestSolution.generateParenthesis(parenthesesPairs);
+
+        System.out.println("[");
+        for (String combination : combinations) {
+            System.out.println("   " + combination);
+        }
+        System.out.println("]");
     }
 }
