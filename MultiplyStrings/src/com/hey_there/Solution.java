4d9acce5136d217f8f7f14064363ab36492f4456
chenwenzhuo <Chenwz1996@163.com>
2019-08-20 16:00:32 +0800

    优化了数字字符串求和的算法，通过

--- MultiplyStrings/src/com/hey_there/Solution.java
+++ MultiplyStrings/src/com/hey_there/Solution.java
@@ -13,38 +13,33 @@ public class Solution {
         //两数的长度
         int len_num1 = num1.length();
         int len_num2 = num2.length();
-        //交换num1和num2所引用的对象，让num2始终指向较短的一个
-        if (len_num1 < len_num2) {
-            String temp = num1;
-            num1 = num2;
-            num2 = temp;
-
-            //同时交换len_num1和len_num2的值
-            len_num1 = num1.length();
-            len_num2 = num2.length();
-        }
 
         List<String> tempResults = new ArrayList<>();
-        for (int i = 1; i <= len_num2; i++) {
+        for (int i = 1; i <= len_num1; i++) {
             StringBuilder tempResult = new StringBuilder();
 
-            //当前循环，num2参与运算的位
-            int num2_digit = Integer.parseInt(num2.charAt(len_num2 - i) + "");
+            //当前循环，num1参与运算的位
+            int num1_digit = Integer.parseInt(num1.charAt(len_num1 - i) + "");
             //当前位为0时简化计算
-            if (num2_digit == 0) {
-                tempResults.add("0");
+            if (num1_digit == 0) {
+                //tempResults.add("0");
                 continue;
             }
 
             int carry = 0;
-            for (int j = 1; j <= len_num1; j++) {
-                //当前循环，num1参与运算的位
-                int num1_digit = Integer.parseInt(num1.charAt(len_num1 - j) + "");
+            for (int j = 1; j <= len_num2; j++) {
+                //当前循环，num2参与运算的位
+                int num2_digit = Integer.parseInt(num2.charAt(len_num2 - j) + "");
 
                 int product = num1_digit * num2_digit + carry;
                 carry = product / 10;
                 tempResult.insert(0, product % 10);
             }
+
+            if (carry != 0) {
+                tempResult.insert(0, carry);
+            }
+
             //在tempResult后加0
             int appendedZeros = 0;
             while (appendedZeros < i - 1) {
@@ -55,14 +50,24 @@ public class Solution {
         }
 
         System.out.println(tempResults);
-        return addStrings(tempResults);
+
+        //将字符串List直接作为参数传入，求和
+        //return addStringsInList(tempResults);
+
+        //将List中的元素逐个传入求和
+        String sum = "0";
+        for (String num : tempResults) {
+            sum = addStrings(sum, num);
+        }
+
+        return sum;
     }
 
-    private String addStrings(List<String> numInString) {
+    private String addStringsInList(List<String> numInString) {
         String sum = "0";//和数字符串
         int len_sum = sum.length();//和数字符串的长度
 
-        int sizeOfList = numInString.size();
+        int sizeOfList = numInString.size();//字符串形式的数字的数量
         for (int i = 0; i < sizeOfList; i++) {
             String currentStr = numInString.get(i);//获取当前参与运算的数字字符串
             //若当前加数为0，跳过计算
@@ -111,11 +116,6 @@ public class Solution {
 
             //退出上方循环后，长度较短的操作数2已经处理完毕，后面仅处理进位与操作数1相加
             for (int j = len_operand_2 + 1; j <= len_operand_1; j++) {
-                //若没有进位，直接退出循环
-                if (carry == 0) {
-                    break;
-                }
-
                 //取操作数1的倒数第j位
                 int operand_1_digit = Integer.parseInt(operand_1.charAt(len_operand_1 - j) + "");
 
@@ -138,5 +138,54 @@ public class Solution {
         }
 
         return sum;
+    }
+
+    private String addStrings(String num1, String num2) {
+        StringBuilder sum = new StringBuilder();
+
+        //计算两数字字符串的长度
+        int len_num1 = num1.length();
+        int len_num2 = num2.length();
+
+        //两数字字符串计算过程中的下标
+        int index_num1 = len_num1 - 1;
+        int index_num2 = len_num2 - 1;
+
+        int carry = 0;//进位
+        while (index_num1 >= 0 || index_num2 >= 0) {
+            //num1和num2参加此次循环计算的位
+            int digit_num1;
+            int digit_num2;
+            if (index_num1 >= 0) {
+//                digit_num1 = Integer.parseInt(num1.charAt(index_num1) + "");
+                digit_num1 = num1.charAt(index_num1) - '0';
+            } else {
+                digit_num1 = 0;
+            }
+            if (index_num2 >= 0) {
+//                digit_num2 = Integer.parseInt(num2.charAt(index_num2) + "");
+                digit_num2 = num2.charAt(index_num2) - '0';
+            } else {
+                digit_num2 = 0;
+            }
+
+            //两个当前位与进位位相加
+            int tempSum = digit_num1 + digit_num2 + carry;
+            carry = tempSum / 10;
+            sum.append(tempSum % 10);
+
+            if (index_num1 >= 0) {
+                index_num1--;
+            }
+            if (index_num2 >= 0) {
+                index_num2--;
+            }
+        }
+
+        if (carry != 0) {
+            sum.append(carry);
+        }
+
+        return sum.reverse().toString();
     }
 }
