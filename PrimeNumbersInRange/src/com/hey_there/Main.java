38ed68b8b8072fc10825048301606bab01530b95
chenwenzhuo <Chenwz1996@163.com>
2019-08-24 17:10:58 +0800

    求指定区间[low,high)内的所有质数，并求所有质数的个位和与十位和，返回其中较小的一个

---
+++ PrimeNumbersInRange/src/com/hey_there/Main.java
@@ -0,0 +1,9 @@
+package com.hey_there;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.primeNumbersInRange(151, 160));
+    }
+}
---
+++ PrimeNumbersInRange/src/com/hey_there/Solution.java
@@ -0,0 +1,40 @@
+package com.hey_there;
+
+public class Solution {
+    public int primeNumbersInRange(int low, int high) {
+        int[] nums = new int[high - 2];
+        for (int i = 0; i < high - 2; i++) {
+            nums[i] = i + 2;
+        }
+
+        for (int i = 0; i < high - 2; i++) {
+            int currentNum_i = nums[i];//当前i位置上的数
+            if (currentNum_i == -1) {
+                continue;
+            }
+
+            for (int j = 0; j < i; j++) {
+                int currentNum_j = nums[j];
+                if (currentNum_j == -1) {
+                    continue;
+                }
+
+                if (currentNum_i % currentNum_j == 0) {
+                    nums[i] = -1;
+                    break;
+                }
+            }
+        }
+
+        //求个位数字的和，与十位数字的和
+        int oneDigitSum = 0, tenDigitSum = 0;
+        for (int i = high - 2 - 1; i >= low - 2; i--) {
+            if (nums[i] != -1) {
+                oneDigitSum += nums[i] % 10;
+                tenDigitSum += (nums[i] / 10) % 10;
+            }
+        }
+
+        return Math.min(oneDigitSum, tenDigitSum);
+    }
+}
