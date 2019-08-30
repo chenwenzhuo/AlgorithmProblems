8904790c99efaa96636ac50014a4d48c8164bf83
chenwenzhuo <Chenwz1996@163.com>
2019-08-30 17:33:50 +0800

    动态规划算法完成了生成n 个结点的所有二叉搜索树

--- UniqueBinarySearchTrees_GenTrees/src/com/hey_there/Solution.java
+++ UniqueBinarySearchTrees_GenTrees/src/com/hey_there/Solution.java
@@ -2,26 +2,71 @@ package com.hey_there;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Solution {
     public List<TreeNode> generateTrees(int n) {
-        List<List<TreeNode>> resultTrees = new ArrayList<>();
+        if (n == 0) {
+            return new ArrayList<>();
+        }
 
-        List<TreeNode> trees_zero = new ArrayList<>();//包含0个结点的数，即仅包含null
-        trees_zero.add(null);
-        resultTrees.add(trees_zero);
+        List<TreeNode>[] resultTrees = new ArrayList[n + 1];
+        resultTrees[0] = new ArrayList<>();
+        resultTrees[0].add(null);
 
-        //range是结点值的范围，即以1~range的构建二叉搜索树
-        for (int range = 1; range <= n; range++) {
-            //rootVal是根结点的值
-            for (int rootVal = 1; rootVal <= range; rootVal++) {
-                int leftTreeLength = rootVal - 1;
-                int rightTreeLength = range - rootVal;
+        for (int len = 1; len <= n; len++) {
+            resultTrees[len] = new ArrayList<>();
 
-                TreeNode rootNode = new TreeNode(rootVal);//根结点
+            for (int rootVal = 1; rootVal <= len; rootVal++) {
+                int leftTree_len = rootVal - 1;
+                int rightTree_len = len - rootVal;
+                List<TreeNode> leftTrees = resultTrees[leftTree_len];
+                List<TreeNode> rightTrees = resultTrees[rightTree_len];
+
+                for (TreeNode leftTree : leftTrees) {
+                    for (TreeNode rightTree : rightTrees) {
+                        TreeNode copiedTree = new TreeNode(rootVal);
+                        copiedTree.left = leftTree;
+                        copiedTree.right = copyTree(rightTree, rootVal);
+
+                        resultTrees[len].add(copiedTree);
+                    }
+                }
             }
         }
 
-        return null;
+        return resultTrees[n];
+    }
+
+    private TreeNode copyTree(TreeNode n, int offset) {
+        if (n == null) {
+            return null;
+        }
+        TreeNode node = new TreeNode(n.val + offset);
+        node.left = copyTree(n.left, offset);
+        node.right = copyTree(n.right, offset);
+        return node;
+    }
+
+    //非递归的先序遍历
+    public List<Integer> non_recursivePreOrderTraversal(TreeNode treeRoot) {
+        List<Integer> traversalResult = new ArrayList<>();//List集合储存遍历结果
+        Stack<TreeNode> nodeStack = new Stack<>();
+        //TreeNode node = treeRoot;
+
+        nodeStack.push(treeRoot);
+        while (!nodeStack.empty()) {
+            TreeNode tempNode = nodeStack.pop();
+            traversalResult.add(tempNode.val);
+
+            if (tempNode.right != null) {
+                nodeStack.push(tempNode.right);
+            }
+            if (tempNode.left != null) {
+                nodeStack.push(tempNode.left);
+            }
+        }
+
+        return traversalResult;
     }
 }
