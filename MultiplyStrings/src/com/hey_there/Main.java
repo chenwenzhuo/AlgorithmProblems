065e437d130d9ad28334c8351246e78048ba7b58
chenwenzhuo <Chenwz1996@163.com>
2019-09-02 16:12:12 +0800

    删除了被移动的目录

--- MultiplyStrings/src/com/hey_there/Main.java
+++
@@ -1,9 +0,0 @@
-package com.hey_there;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.multiply("3245", "57456"));
-    }
-}
--- RomanToInteger/src/com/heythere/Main.java
+++
@@ -1,9 +0,0 @@
-package com.heythere;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Solutions solutions = new Solutions();
-        System.out.println(solutions.romanToInt("MCMXCIV"));
-    }
-}
