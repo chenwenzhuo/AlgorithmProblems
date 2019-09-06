dedee2df439dd43a6f3356d7d3bf46f627095e65
chenwenzhuo <Chenwz1996@163.com>
2019-09-06 11:29:02 +0800

    ConstructBinaryTreeFromPreorderInorderTraversal 改进算法，用HashMap存储中序遍历值到下标的映射，节省寻找下标的时间，递归方法中仅传递inOrder序列的左右界，节省空间消耗

--- LeetCode/src/com/hey_there/ConstructBinaryTreeFromPreorderInorderTraversal/Solution_recursive.java
+++ LeetCode/src/com/hey_there/ConstructBinaryTreeFromPreorderInorderTraversal/Solution_recursive.java
@@ -2,9 +2,43 @@ package com.hey_there.ConstructBinaryTreeFromPreorderInorderTraversal;
 
 import com.hey_there.BinaryTreeInOrderTraversal.TreeNode;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Solution_recursive {
+    private Map<Integer, Integer> inOrder_value2Index = new HashMap<>();
+    private int[] preOrder;
+    private int preIndex = 0;
+
+    /*对最初的递归算法进行改进，
+     * 用HashMap存储中序遍历值到下标的映射，节省寻找下标的时间*/
+    public TreeNode buildTree_improved(int[] preOrder, int[] inOrder) {
+        this.preOrder = preOrder;
+
+        //储存映射
+        int inIndex = 0;
+        for (int inVal : inOrder) {
+            inOrder_value2Index.put(inVal, inIndex);
+            inIndex++;
+        }
+
+        return buildHelper(0, inOrder.length);
+    }
+
+    private TreeNode buildHelper(int inLeft, int inRight) {
+        if (inLeft == inRight) {
+            return null;
+        }
+
+        int currentRootVal = preOrder[preIndex++];//当前树的根结点值
+        TreeNode currentRoot = new TreeNode(currentRootVal);//当前树的根结点
+
+        int curRoot_inOrderIndex = inOrder_value2Index.get(currentRootVal);//当前根结点在中序遍历序列中的下标
+
+        currentRoot.left = buildHelper(inLeft, curRoot_inOrderIndex);
+        currentRoot.right = buildHelper(curRoot_inOrderIndex + 1, inRight);
+        return currentRoot;
+    }
+
     public TreeNode buildTree(int[] preOrder, int[] inOrder) {
         if (preOrder.length == 0 || inOrder.length == 0) {
             return null;
@@ -12,9 +46,6 @@ public class Solution_recursive {
 
         TreeNode currentRoot = new TreeNode(preOrder[0]);//当前树的根结点
         int indexOfRootVal_inOrder = indexOfValue(inOrder, preOrder[0]);//当前树的根结点值在中序遍历序列中的下标
-        if (indexOfRootVal_inOrder == -1) {
-            System.out.println("下标值为-1！");
-        }
 
         //获取当前树的左子树的中序遍历结果
         int[] currentTree_leftChildren_inOrder =
@@ -50,6 +81,6 @@ public class Solution_recursive {
         Solution_recursive solutionRecursive = new Solution_recursive();
         int[] preOrder = {3, 9, 20, 15, 7};
         int[] inOrder = {9, 3, 15, 20, 7};
-        System.out.println(solutionRecursive.buildTree(preOrder, inOrder).val);
+        System.out.println(solutionRecursive.buildTree_improved(preOrder, inOrder).val);
     }
 }
