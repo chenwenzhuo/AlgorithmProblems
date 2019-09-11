a1c03976451eb4481be0550a6f336faed3045f83
chenwenzhuo <Chenwz1996@163.com>
2019-09-11 17:15:06 +0800

    PathSumEqualsTargetSum_findAllPaths 单步跑了代码，理清了思路

--- LeetCode/src/com/hey_there/PathSumEqualsTargetSum_findAllPaths/Solution.java
+++ LeetCode/src/com/hey_there/PathSumEqualsTargetSum_findAllPaths/Solution.java
@@ -26,26 +26,9 @@ public class Solution {
         }
 
         int currentSum = node.val + prevSum;//根结点到当前结点的路径之和
-        /*//若路径和等于目标值，且当前结点是叶子
-        if (currentSum == targetSum && node.left == null && node.right == null) {
-            //新建一个List，加入路径上所有结点的值
-            List<Integer> onePath = new ArrayList<>(prevList);
-            onePath.add(node.val);//将当前结点值加入onePath
-            allResults.add(onePath);//将当前路径加入结果集合
-            return;
-        }
-
-        //新建一个List，加入根结点到当前结点的路径上所有结点的值
-        List<Integer> nextList = new ArrayList<>(prevList);
-        nextList.add(node.val);
-        //继续递归
-        helper(node.left, currentSum, nextList);
-        helper(node.right, currentSum, nextList);*/
-
         prevList.add(node.val);
         if (currentSum == targetSum && node.left == null && node.right == null) {
             allResults.add(new ArrayList<>(prevList));
-            //return;
         }
         helper(node.left, currentSum, prevList);
         helper(node.right, currentSum, prevList);
