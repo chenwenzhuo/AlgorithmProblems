25e5b221af02e65813af7ebf7b68908a95cb3b58
chenwenzhuo <Chenwz1996@163.com>
2019-09-17 11:32:18 +0800

    非递归的后序遍历实现

--- LeetCode/src/com/hey_there/BinaryTreePostOrderTraversal/Solution.java
+++ LeetCode/src/com/hey_there/BinaryTreePostOrderTraversal/Solution.java
@@ -1,4 +1,61 @@
 package com.hey_there.BinaryTreePostOrderTraversal;
 
+import com.hey_there.BinaryTreeInOrderTraversal.TreeNode;
+
+import java.util.*;
+
 public class Solution {
+    public List<Integer> non_recursivePostOrderTraversal(TreeNode treeRoot) {
+        List<Integer> traversalResult = new ArrayList<>();//List集合储存遍历结果
+        Stack<TreeNode> nodeStack = new Stack<>();
+        //以键值对的形式保存结点是否被访问过的标记
+        Map<TreeNode, Boolean> mark_nodeVisited = new HashMap<>();
+
+        TreeNode node;
+        nodeStack.push(treeRoot);
+        while (!nodeStack.empty()) {
+            node = nodeStack.peek();
+
+            if ((node.left == null && node.right == null) || childrenVisited(node, mark_nodeVisited)) {
+                TreeNode tempNode = nodeStack.pop();
+                traversalResult.add(tempNode.val);
+
+                mark_nodeVisited.put(tempNode, true);//将访问过的结点标记为true
+            } else {
+                if (node.right != null) {
+                    nodeStack.push(node.right);
+                    mark_nodeVisited.put(node.right, false);//将首次入栈的结点标记为false
+                }
+                if (node.left != null) {
+                    nodeStack.push(node.left);
+                    mark_nodeVisited.put(node.left, false);
+                }
+            }
+        }
+
+        return traversalResult;
+    }
+
+    private boolean childrenVisited(TreeNode node, Map<TreeNode, Boolean> marks) {
+        boolean leftVisited = false;
+        boolean rightVisited = false;
+
+        if (node.left == null) {
+            leftVisited = true;
+        } else {
+            if (marks.containsKey(node.left)) {
+                leftVisited = marks.get(node.left);
+            }
+        }
+
+        if (node.right == null) {
+            rightVisited = true;
+        } else {
+            if (marks.containsKey(node.right)) {
+                rightVisited = marks.get(node.right);
+            }
+        }
+
+        return leftVisited && rightVisited;
+    }
 }
