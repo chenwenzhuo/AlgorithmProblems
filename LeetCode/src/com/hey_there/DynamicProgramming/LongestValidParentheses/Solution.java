e6f87ee150fd175e8734687096bb4c626740eb3b
chenwenzhuo <Chenwz1996@163.com>
2019-10-10 14:40:00 +0800

    LongestValidParentheses debug完成

--- LeetCode/src/com/hey_there/DynamicProgramming/LongestValidParentheses/Solution.java
+++ LeetCode/src/com/hey_there/DynamicProgramming/LongestValidParentheses/Solution.java
@@ -5,46 +5,46 @@ import java.util.Map;
 
 public class Solution {
     public int longestValidParentheses(String s) {
-        //s中可能有多个有效括号串，将其最后字符下标作为key，括号串作为value存入validParenthesesStrings
-        HashMap<Integer, String> validParenthesesStrings = new HashMap<>();
+        //s中可能有多个有效括号串，将其最后字符下标作为key，括号串长度作为value存入validParenthesesStrings
+        HashMap<Integer, Integer> validParenthesesStrings = new HashMap<>();
 
         //最大有效括号长度
         int maxLength = 0;//可能不存在有效括号，故初始化为0
 
-        //下一对有效括号的左括号的下标
-        int index_nextValidPair = -1;//s中可能没有有效括号对，故将其初始化为-1
+        int index_nextValidPair;//下一对有效括号的左括号的下标
+        int validParenthesesStart;//有效括号串的起始字符
+        int validParenthesesEnd = -1;//有效括号串的结束字符，初始情况下还未发现有效括号串，将其初始化为-1
 
         while (true) {
-            index_nextValidPair = nextValidPairIndex(index_nextValidPair + 1, s);
+            //从前一个有效括号串的下一个字符开始寻找下一对有效括号对
+            index_nextValidPair = nextValidPairIndex(validParenthesesEnd + 1, s);
             if (index_nextValidPair == -1) {
-                //若index_nextValidPair值未更新，说明s中没有后续有效括号对，退出循环
+                //若index_nextValidPair值为-1，说明s中没有后续有效括号对，退出循环
                 break;
             }
 
             //若找到一对有效括号对
-            /*以第一对有效括号为基础进行扩张，
+            /*以此有效括号对为基础进行扩张，
              * 若字符串 str 为有效括号串，则扩张 str 的方法有两种：
              * 1.str()
              * 2.(str)*/
-            StringBuilder buildValidParentheses = new StringBuilder("()");
-            int validParenthesesStart = index_nextValidPair;//有效括号串的起始字符
-            int validParenthesesEnd = index_nextValidPair + 1;//有效括号串的结束字符
+            //更新有效括号串的起始终止字符下标值
+            validParenthesesStart = index_nextValidPair;//有效括号串的起始字符
+            validParenthesesEnd = index_nextValidPair + 1;//有效括号串的结束字符
 
             while (validParenthesesStart >= 0 && validParenthesesEnd < s.length()) {//当括号串的范围不超过s时，继续扩张
                 //检查后续字符
                 if (validParenthesesEnd + 3 <= s.length() &&//首先检查是否会越界
                         s.substring(validParenthesesEnd + 1, validParenthesesEnd + 3).equals("()")) {
-                    //若后续两个字符是括号对，将其加入buildValidParentheses中，更新validParenthesesEnd
-                    buildValidParentheses.append("()");
+                    //若后续两个字符是括号对，则当前串可向后扩张，更新validParenthesesEnd的值
                     validParenthesesEnd += 2;
                     continue;
                 }
 
                 if (validParenthesesStart - 1 >= 0 && validParenthesesEnd + 1 < s.length() &&//检查是否越界
                         s.charAt(validParenthesesStart - 1) == '(' && s.charAt(validParenthesesEnd + 1) == ')') {
-                    //若前一个字符和后一个字符构成括号对包围当前串
-                    buildValidParentheses.insert(0, "(");
-                    buildValidParentheses.append(")");
+                    //若前一个字符和后一个字符构成括号对包围当前串，则当前串可向左右两个方向扩张
+                    //更新validParenthesesStart和validParenthesesEnd的值
                     validParenthesesStart -= 1;
                     validParenthesesEnd += 1;
                     continue;
@@ -53,20 +53,18 @@ public class Solution {
                 //若当前串无法继续扩张
                 //首先检查当前串是否能与前面的有效串拼接
                 if (validParenthesesStrings.containsKey(validParenthesesStart - 1)) {
-                    //若能拼接，将当前串与前方串进行拼接，更新validParenthesesStart
-                    String prev = validParenthesesStrings.get(validParenthesesStart - 1);
-                    buildValidParentheses.insert(0, prev);
-                    validParenthesesStart -= prev.length();
+                    //若能拼接，将当前串与前方串进行拼接，当前串向左扩张，更新validParenthesesStart
+                    validParenthesesStart -= validParenthesesStrings.get(validParenthesesStart - 1);
 
                     //完成拼接后，继续尝试扩张
                     continue;
                 }
 
                 //若无法扩张也无法拼接
-                maxLength = Math.max(maxLength, buildValidParentheses.length());//更新最长串的长度值
-                validParenthesesStrings.put(validParenthesesEnd, buildValidParentheses.toString());//将当前串存入map
-
-                buildValidParentheses.delete(0, buildValidParentheses.length());//将builder清空
+                int currentParenthesesLength = validParenthesesEnd - validParenthesesStart + 1;//计算当前串的长度
+                maxLength = Math.max(maxLength, currentParenthesesLength);//更新最长串的长度值
+                //将当前串存入map
+                validParenthesesStrings.put(validParenthesesEnd, currentParenthesesLength);
                 break;
             }
         }
