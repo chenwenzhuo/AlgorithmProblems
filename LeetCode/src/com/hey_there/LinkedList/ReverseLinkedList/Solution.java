a2f7b60b3ea71c009cce9d1d1c2276c819bb64ed
chenwenzhuo <Chenwz1996@163.com>
2019-12-25 10:13:16 +0800

    ReverseLinkedList & ReverseLinkedList_2

---
+++ LeetCode/src/com/hey_there/LinkedList/ReverseLinkedList/Solution.java
@@ -0,0 +1,29 @@
+package com.hey_there.LinkedList.ReverseLinkedList;
+
+import com.hey_there.LinkedList.ListNode;
+
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode reversedHead = new ListNode(-1);
+
+        while (head != null) {
+            ListNode nextNode = head.next;
+
+            head.next = reversedHead.next;
+            reversedHead.next = head;
+
+            head = nextNode;
+        }
+        return reversedHead.next;
+    }
+
+    public ListNode reverseList_recursive(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode node = reverseList_recursive(head.next);
+        head.next.next = head;
+        head.next = null;
+        return node;
+    }
+}
