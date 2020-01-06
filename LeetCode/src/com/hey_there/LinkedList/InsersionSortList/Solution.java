d7c47b1a68161f3a4e333a5c69d0d3713593f557
chenwenzhuo <Chenwz1996@163.com>
2020-01-06 21:48:03 +0800

    InsertionSortList 实现了两层嵌套循环的O(n^2)复杂度的算法，记录了两个更高效的算法

---
+++ LeetCode/src/com/hey_there/LinkedList/InsersionSortList/Solution.java
@@ -0,0 +1,62 @@
+package com.hey_there.LinkedList.InsersionSortList;
+
+import com.hey_there.LinkedList.ListNode;
+
+import java.util.*;
+
+public class Solution {
+    //两层嵌套循环算法
+    public ListNode insertionSortList_nestedLoop(ListNode head) {
+        //给原链表加上头结点，方便操作
+        ListNode headNode = new ListNode(Integer.MIN_VALUE);
+        headNode.next = head;
+
+        ListNode prevNode = headNode;//head指向的结点的前一个结点的引用，初始指向头结点
+        int prevNodeVal = headNode.val;//head指向的结点的前一个结点的值，初始为头结点的值
+        //遍历链表
+        while (head != null) {
+            int curNodeVal = head.val;//当前结点的值
+            //若当前结点值大于等于前一结点的值，则不进行排序
+            if (curNodeVal >= prevNodeVal) {
+                prevNode = head;//更新prevNode的指向
+                prevNodeVal = head.val;//更新prevNodeVal的值
+                head = head.next;//head指向下一结点
+                continue;
+            }
+
+            ListNode prevListRef = headNode;
+            while (prevListRef != head) {//从头遍历链表，寻找插入位置
+                //若找到插入位置
+                if (prevListRef.val <= curNodeVal && prevListRef.next.val >= curNodeVal) {
+                    //将当前结点从后方链表上取下
+                    prevNode.next = head.next;
+
+                    //将当前结点插入正确位置
+                    head.next = prevListRef.next;
+                    prevListRef.next = head;
+                    break;
+                }
+                prevListRef = prevListRef.next;
+            }
+            head = prevNode.next;//完成插入后更新head的值
+        }
+        return headNode.next;
+    }
+
+
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(3);
+
+        Solution solution = new Solution();
+        head = solution.insertionSortList_nestedLoop(head);
+
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+}
