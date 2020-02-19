f8fecbd7a58c093ede0abed601530f263728858f
chenwenzhuo <Chenwz1996@163.com>
2020-02-19 17:45:36 +0800

    RemoveDuplicatesFromSortedArray

---
+++ LeetCode/src/com/hey_there/Array/RemoveDuplicatesFromSortedArray/Solution.java
@@ -0,0 +1,17 @@
+package com.hey_there.Array.RemoveDuplicatesFromSortedArray;
+
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int len = nums.length;//数组长度
+        int actualIndex = 1, duplicateIndex;//用两个下标表示数组中元素
+
+        for (duplicateIndex = 1; duplicateIndex < len; duplicateIndex++) {
+            if (nums[duplicateIndex] == nums[duplicateIndex - 1]) {
+                continue;
+            }
+            nums[actualIndex] = nums[duplicateIndex];
+            actualIndex++;
+        }
+        return actualIndex;
+    }
+}
