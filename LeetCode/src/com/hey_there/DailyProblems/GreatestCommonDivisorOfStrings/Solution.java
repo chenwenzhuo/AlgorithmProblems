bac0c1b77009364d2b30cc8c5c43659752e85d96
chenwenzhuo <Chenwz1996@163.com>
2020-03-12 20:55:47 +0800

    LeetCode每日一题 GreatestCommonDivisorOfStrings 更改了求最大公约数的算法

--- LeetCode/src/com/hey_there/DailyProblems/GreatestCommonDivisorOfStrings/Solution.java
+++ LeetCode/src/com/hey_there/DailyProblems/GreatestCommonDivisorOfStrings/Solution.java
@@ -1,7 +1,7 @@
 package com.hey_there.DailyProblems.GreatestCommonDivisorOfStrings;
 
 public class Solution {
-    public String gcdOfStrings(String str1, String str2) {
+    public static String gcdOfStrings(String str1, String str2) {
         int len1 = str1.length(), len2 = str2.length();//两个字符串的长度
 
         //str1+str2与str2+str1相同时有解
@@ -11,16 +11,26 @@ public class Solution {
             return "";//若两拼接字符串不相同，则无解
         }
 
-        //取两长度的较小者
-        int shorter = Math.min(len1, len2);
-        //求长度的最大公约数
+        //求两长度的最大公约数
+        int bigger = Math.max(len1, len2);
+        int smaller = Math.min(len1, len2);
         int gcd;
-        while (shorter > 0) {
-            if (len1 % shorter == 0 && len2 % shorter == 0) {
+        //辗转相除法
+        while (true) {
+            int remainder = bigger % smaller;
+            if (remainder == 0) {
+                gcd = smaller;
                 break;
             }
-            shorter--;
+            bigger = smaller;
+            smaller = remainder;
         }
-        return str1.substring(0, shorter);
+        return str1.substring(0, gcd);
+    }
+
+    public static void main(String[] args) {
+        String str1 = "ABCABC";
+        String str2 = "ABC";
+        System.out.println(gcdOfStrings(str1, str2));
     }
 }
