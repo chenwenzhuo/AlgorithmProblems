65426b4019a8381fcf2764a4cce152c9f39fd0d3
chenwenzhuo <Chenwz1996@163.com>
2020-04-17 12:03:56 +0800

    JumpGame_2添加了一种解法

--- LeetCode/src/com/hey_there/Array/JumpGame_2/Solution.java
+++ LeetCode/src/com/hey_there/Array/JumpGame_2/Solution.java
@@ -1,7 +1,9 @@
 package com.hey_there.Array.JumpGame_2;
 
+import java.util.Arrays;
+
 public class Solution {
-    public int jump_my(int[] nums) {
+    public int jump_my_1(int[] nums) {
         int totalSteps = 0;
         int len = nums.length;
 
@@ -33,6 +35,29 @@ public class Solution {
         return totalSteps;
     }
 
+    public int jump_my_2(int[] nums) {
+        int lenNums = nums.length;
+        //dp[i]表示到达下标为i的位置需要的最少步数
+        int[] dp = new int[lenNums];
+        Arrays.fill(dp, lenNums);//到达任意一个位置，最多不超过lenNums步
+        dp[0] = 0;//初始位置所需步数为0
+
+        //计算每一个dp[i]
+        for (int i = 0; i < lenNums; i++) {
+            int steps = 1;
+            while (steps <= nums[i]) {
+                int dest = i + steps;
+                if (dest >= lenNums) {
+                    //从当前位置走steps步后若越界则不再继续尝试
+                    break;
+                }
+                dp[dest] = Math.min(dp[dest], dp[i] + 1);
+                steps++;
+            }
+        }
+        return dp[lenNums - 1];
+    }
+
     public int jump(int[] nums) {
         int end = 0;
         int maxPosition = 0;
@@ -51,6 +76,6 @@ public class Solution {
     public static void main(String[] args) {
         int[] nums = {2, 3, 1, 1, 4};
         Solution solution = new Solution();
-        System.out.println(solution.jump(nums));
+        System.out.println(solution.jump_my_2(nums));
     }
 }
