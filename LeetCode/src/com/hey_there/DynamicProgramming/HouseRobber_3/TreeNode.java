b09f7d656762a5999e1ed2e01b708b9e4acd341b
chenwenzhuo <Chenwz1996@163.com>
2020-04-21 17:17:28 +0800

    HouseRobber-3

---
+++ LeetCode/src/com/hey_there/DynamicProgramming/HouseRobber_3/TreeNode.java
@@ -0,0 +1,11 @@
+package com.hey_there.DynamicProgramming.HouseRobber_3;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
