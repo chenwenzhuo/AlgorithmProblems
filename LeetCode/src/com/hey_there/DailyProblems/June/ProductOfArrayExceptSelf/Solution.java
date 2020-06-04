c9f12143cf901acef20397fc6b2a590ce4a1e925
chenwenzhuo <Chenwz1996@163.com>
2020-06-04 16:10:33 +0800

    LeetCode每日一题 ProductOfArrayExceptSelf

---
+++ LeetCode/src/com/hey_there/DailyProblems/June/ProductOfArrayExceptSelf/Solution.java
@@ -0,0 +1,32 @@
+package com.hey_there.DailyProblems.June.ProductOfArrayExceptSelf;
+
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        //leftProd[i]表示nums[0]~nums[i-1]闭区间内所有数的乘积
+        //规定leftProd[0]为1
+        int[] leftProd = new int[len];
+        leftProd[0] = 1;
+        for (int i = 1; i < len; i++) {
+            leftProd[i] = leftProd[i - 1] * nums[i - 1];
+        }
+        //rightProd表示从后往前所有数的乘积
+        int rightProd = nums[len - 1];
+        for (int i = len - 2; i >= 0; i--) {
+            leftProd[i] *= rightProd;
+            rightProd *= nums[i];
+        }
+        return leftProd;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        Solution solution = new Solution();
+        int[] ans = solution.productExceptSelf(nums);
+
+        for (int a : ans) {
+            System.out.print(a + "   ");
+        }
+        System.out.println();
+    }
+}
