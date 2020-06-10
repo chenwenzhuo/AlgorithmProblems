7275f1409683641656944cbf8273a4c3fb9f0719
chenwenzhuo <Chenwz1996@163.com>
2020-06-11 01:14:18 +0800

    LeetCode每日一题 DailyTemperatures

--- LeetCode/src/com/hey_there/DailyProblems/June/DailyTemperatures/Solution.java
+++ LeetCode/src/com/hey_there/DailyProblems/June/DailyTemperatures/Solution.java
@@ -31,4 +31,30 @@ public class Solution {
         }
         return ans;
     }
+
+    public int[] dailyTemperatures(int[] T) {
+        int[] result = new int[T.length];
+        for (int i = T.length - 2; i >= 0; i--) {
+            if (T[i] < T[i + 1]) {
+                result[i] = 1;
+            } else if (T[i] == T[i + 1]) {
+                if (result[i + 1] != 0) {
+                    result[i] = result[i + 1] + 1;
+                }
+            } else {
+                for (int index = i + 1; ; ) {
+                    index += result[index];
+                    if (T[i] < T[index]) {
+                        result[i] = index - i;
+                        break;
+                    }
+                    if (result[index] == 0) {
+                        break;
+                    }
+                }
+
+            }
+        }
+        return result;
+    }
 }
