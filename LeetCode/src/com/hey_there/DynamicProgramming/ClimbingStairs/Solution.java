008894b0ef6af230c868c7524fbb197533bb1ded
chenwenzhuo <Chenwz1996@163.com>
2020-06-13 00:30:17 +0800

    LeetCode每日一题 ClimbStairs 修改了代码，降低了空间复杂度

--- LeetCode/src/com/hey_there/DynamicProgramming/ClimbingStairs/Solution.java
+++ LeetCode/src/com/hey_there/DynamicProgramming/ClimbingStairs/Solution.java
@@ -21,6 +21,23 @@ public class Solution {
         return waysToCurStair;
     }
 
+    public int climbStairs_1(int n) {
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        }
+        //到达前一级台阶的方式数
+        int prevOne = 2;
+        //到达前两级台阶的方式数
+        int prevTwo = 1;
+        for (int i = 2; i < n; i++) {
+            prevOne += prevTwo;
+            prevTwo = prevOne - prevTwo;
+        }
+        return prevOne;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         int ans = solution.climbStairs(4);
