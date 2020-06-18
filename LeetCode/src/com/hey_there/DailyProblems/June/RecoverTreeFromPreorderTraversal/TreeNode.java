9c0ad44c1b1d114526d93c1bc4bb738c4d532fe6
chenwenzhuo <Chenwz1996@163.com>
2020-06-18 13:23:06 +0800

    LeetCode每日一题 RecoverTreeFromPreorderTraversal

---
+++ LeetCode/src/com/hey_there/DailyProblems/June/RecoverTreeFromPreorderTraversal/TreeNode.java
@@ -0,0 +1,11 @@
+package com.hey_there.DailyProblems.June.RecoverTreeFromPreorderTraversal;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
