4b669574cbadcd2d0ca0259bba82ecea480caed5
chenwenzhuo <Chenwz1996@163.com>
2020-07-09 11:28:23 +0800

    TwoSum_2_InputArraySorted

---
+++ LeetCode/src/com/hey_there/TwoSum_2_InputArraySorted/Solution.java
@@ -0,0 +1,22 @@
+package com.hey_there.TwoSum_2_InputArraySorted;
+
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] res = new int[2];
+        int left = 0, right = numbers.length - 1;
+        while (left < right) {
+            if (numbers[left] + numbers[right] < target) {
+                left++;
+                continue;
+            } else if (numbers[left] + numbers[right] > target) {
+                right--;
+                continue;
+            }
+            //返回的是两数在数组中的顺序，不是下标，所以需要加1
+            res[0] = left + 1;
+            res[1] = right + 1;
+            break;
+        }
+        return res;
+    }
+}
