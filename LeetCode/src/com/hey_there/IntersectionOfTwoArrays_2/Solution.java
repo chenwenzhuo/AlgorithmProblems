35bea6539e87ccd4d741157bea0123177e4a9ac1
chenwenzhuo <Chenwz1996@163.com>
2020-07-13 14:41:41 +0800

    IntersectionOfTwoArrays_2

--- LeetCode/src/com/hey_there/IntersectionOfTwoArrays_2/Solution.java
+++ LeetCode/src/com/hey_there/IntersectionOfTwoArrays_2/Solution.java
@@ -2,9 +2,10 @@ package com.hey_there.IntersectionOfTwoArrays_2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Solution {
-    public int[] intersect(int[] nums1, int[] nums2) {
+    public int[] intersect_1(int[] nums1, int[] nums2) {
         int len1 = nums1.length, len2 = nums2.length;
         Arrays.sort(nums1);
         Arrays.sort(nums2);
@@ -22,8 +23,39 @@ public class Solution {
             }
         }
         int[] intersectionArr = new int[intersection.size()];
-        for (int i = 0; i < intersectionArr.length; i++) {
-            intersectionArr[i] = intersection.get(i);
+        int idx = 0;
+        for (int n : intersection) {
+            intersectionArr[idx++] = n;
+        }
+        return intersectionArr;
+    }
+    public int[] intersect_2(int[] nums1, int[] nums2) {
+        int len1 = nums1.length, len2 = nums2.length;
+        //将nums1数组所有元素存入HashMap中
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int n1 : nums1) {
+            int count = map.getOrDefault(n1, 0) + 1;
+            map.put(n1, count);
+        }
+
+        ArrayList<Integer> intersection = new ArrayList<>(Math.min(len1, len2));
+        //枚举nums2中所有元素
+        for (int n2 : nums2) {
+            int count = map.getOrDefault(n2, 0);//获取n2在nums1中的出现次数
+            if (count > 0) {
+                intersection.add(n2);
+                count--;
+                if (count > 0) {
+                    map.put(n2, count);
+                } else {
+                    map.remove(n2);
+                }
+            }
+        }
+        int[] intersectionArr = new int[intersection.size()];
+        int idx = 0;
+        for (int n : intersection) {
+            intersectionArr[idx++] = n;
         }
         return intersectionArr;
     }
