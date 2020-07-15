e3e8d838d7316f6b340a2dfb3b88de693cdf918d
chenwenzhuo <Chenwz1996@163.com>
2020-07-15 10:55:27 +0800

    UniqueBinarySearchTrees_Count 添加了注释

--- LeetCode/src/com/hey_there/UniqueBinarySearchTrees_Count/Solution.java
+++ LeetCode/src/com/hey_there/UniqueBinarySearchTrees_Count/Solution.java
@@ -1,17 +1,22 @@
 package com.hey_there.UniqueBinarySearchTrees_Count;
 
 public class Solution {
+    /* 此处不同二叉搜索树的数量只与节点的取值范围有关，
+     * 即，以1~k为节点值的不同BST数量，与以k+1~2k为节点值的不同BST数量相等*/
     public int numTrees(int n) {
-        int[] treeCounts = new int[n + 1];
-        treeCounts[0] = 1;
+        //treeCounts为dp数组，
+        //treeCount[i]表示以1~i为节点值的不同BST数量
+        int[] treeCount = new int[n + 1];
+        treeCount[0] = 1;//规定treeCount[i]为1
 
         for (int i = 1; i <= n; i++) {
-            treeCounts[i] = 0;
+            //枚举1~i范围内的所有数，累加它们作为根时的BST数量
             for (int j = 1; j <= i; j++) {
-                treeCounts[i] += treeCounts[j - 1] * treeCounts[i - j];
+                //treeCount[j - 1]是以1~j-1为节点值的BST数量
+                //treeCount[i - j]是以j+1~i为节点值的BST数量
+                treeCount[i] += treeCount[j - 1] * treeCount[i - j];
             }
         }
-
-        return treeCounts[n];
+        return treeCount[n];
     }
 }
