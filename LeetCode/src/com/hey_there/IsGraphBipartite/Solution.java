7e08e462773ddae78e0fbca24d7c483637366f97
chenwenzhuo <Chenwz1996@163.com>
2020-07-16 14:34:48 +0800

    IsGraphBipartite

---
+++ LeetCode/src/com/hey_there/IsGraphBipartite/Solution.java
@@ -0,0 +1,47 @@
+package com.hey_there.IsGraphBipartite;
+
+import java.util.Arrays;
+
+public class Solution {
+    private static final int UNCOLORED = 0;
+    private static final int RED = 1;
+    private static final int GREEN = 2;
+    private int[] color;
+    private boolean valid;
+
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        valid = true;
+        color = new int[n];
+        Arrays.fill(color, UNCOLORED);
+        for (int i = 0; i < n && valid; ++i) {
+            if (color[i] == UNCOLORED) {
+                dfs(i, RED, graph);
+            }
+        }
+        return valid;
+    }
+
+    public void dfs(int node, int c, int[][] graph) {
+        color[node] = c;
+        int cNei = c == RED ? GREEN : RED;
+        for (int neighbor : graph[node]) {
+            if (color[neighbor] == UNCOLORED) {
+                dfs(neighbor, cNei, graph);
+                if (!valid) {
+                    return;
+                }
+            } else if (color[neighbor] != cNei) {
+                valid = false;
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] graph = {{1}, {0, 3}, {3}, {1, 2}};
+        Solution solution = new Solution();
+        boolean res = solution.isBipartite(graph);
+        System.out.println(res);
+    }
+}
