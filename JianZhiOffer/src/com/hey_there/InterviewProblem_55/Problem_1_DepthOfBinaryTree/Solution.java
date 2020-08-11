6cb4fb53da5f22eb5176b21b2776245dcd8f0b80
chenwenzhuo <Chenwz1996@163.com>
2020-08-11 15:38:15 +0800

    Problem_1_DepthOfBinaryTree

---
+++ JianZhiOffer/src/com/hey_there/InterviewProblem_55/Problem_1_DepthOfBinaryTree/Solution.java
@@ -0,0 +1,10 @@
+package com.hey_there.InterviewProblem_55.Problem_1_DepthOfBinaryTree;
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
---
+++ JianZhiOffer/src/com/hey_there/InterviewProblem_55/Problem_2_BalancedBinaryTree/TreeNode.java
@@ -0,0 +1,10 @@
+package com.hey_there.InterviewProblem_55.Problem_2_BalancedBinaryTree;
+
+public class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
