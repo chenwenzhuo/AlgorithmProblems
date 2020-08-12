4df5003560c84ef9e598a03367c1199d924916de
chenwenzhuo <Chenwz1996@163.com>
2020-08-12 16:03:07 +0800

    InterviewProblem_56_OccurrenceTimesOfNumberInArray_Problem_2

---
+++ JianZhiOffer/src/com/hey_there/InterviewProblem_56_OccurrenceTimesOfNumberInArray/Problem_2/Solution.java
@@ -0,0 +1,12 @@
+package com.hey_there.InterviewProblem_56_OccurrenceTimesOfNumberInArray.Problem_2;
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int ones = 0, twos = 0;
+        for (int num : nums) {
+            ones = ones ^ num & ~twos;
+            twos = twos ^ num & ~ones;
+        }
+        return ones;
+    }
+}
