1d19529f3025466fcef22a9a98d487750d434cd3
chenwenzhuo <Chenwz1996@163.com>
2020-08-14 10:41:49 +0800

    InterviewProblem_57_FindTwoNumbersWithSpecifiedSum

---
+++ JianZhiOffer/src/com/hey_there/InterviewProblem_57_FindTwoNumbersWithSpecifiedSum/Solution.java
@@ -0,0 +1,15 @@
+package com.hey_there.InterviewProblem_57_FindTwoNumbersWithSpecifiedSum;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum < target) left++;
+            else if (sum > target) right--;
+            else break;
+        }
+        if (left >= right) return null;
+        return new int[]{nums[left], nums[right]};
+    }
+}
