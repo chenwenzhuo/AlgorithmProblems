b2fd0349a3287ee44f8a8d0c4f0f9d07eb3e0c20
chenwenzhuo <Chenwz1996@163.com>
2021-07-07 21:35:07 +0800

    CountGoodMeals

---
+++ LeetCode/src/com/hey_there/CountGoodMeals/Solution.java
@@ -0,0 +1,25 @@
+package com.hey_there.CountGoodMeals;
+
+import java.util.HashMap;
+
+public class Solution {
+    public int countPairs(int[] deliciousness) {
+        final int MOD = 1000000007;
+        int maxVal = 0;
+        for (int val : deliciousness) {
+            maxVal = Math.max(maxVal, val);
+        }
+        int maxSum = maxVal * 2;
+        int pairs = 0;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int n = deliciousness.length;
+        for (int val : deliciousness) {
+            for (int sum = 1; sum <= maxSum; sum <<= 1) {
+                int count = map.getOrDefault(sum - val, 0);
+                pairs = (pairs + count) % MOD;
+            }
+            map.put(val, map.getOrDefault(val, 0) + 1);
+        }
+        return pairs;
+    }
+}
