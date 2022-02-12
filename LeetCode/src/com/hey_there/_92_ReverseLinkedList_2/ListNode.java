f37c4b096dc948789ff877b6d62ff483eeb3bb51
chenwenzhuo <Chenwz1996@163.com>
2022-02-12 14:23:54 +0800

    增加一个可传入next参数的构造方法

---
+++ LeetCode/src/com/hey_there/_92_ReverseLinkedList_2/ListNode.java
@@ -0,0 +1,15 @@
+package com.hey_there._92_ReverseLinkedList_2;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
