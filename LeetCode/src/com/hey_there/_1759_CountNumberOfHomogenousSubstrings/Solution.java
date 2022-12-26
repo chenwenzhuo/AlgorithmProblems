9e6c20f613eac83c1d77eb73fb6b15b6acab3b36
chenwenzhuo <Chenwz1996@163.com>
2022-12-26 20:14:41 +0800

    统计同构子字符串的数目

--- LeetCode/src/com/hey_there/_1759_CountNumberOfHomogenousSubstrings/Solution.java
+++ LeetCode/src/com/hey_there/_1759_CountNumberOfHomogenousSubstrings/Solution.java
@@ -4,15 +4,15 @@ public class Solution {
     public int countHomogenous(String s) {
         long res = 0;
         int left = 0, right = 0;
-        final long MOD = 1000000007;
+        final int MOD = 1000000007;
         while (right < s.length()) {
             //移动right，直到right位置的字符与left不同
             while (right < s.length() && s.charAt(left) == s.charAt(right))
                 right++;
             long curLen = right - left;//相同字符构成的连续子串的长度
-            res = res + (curLen * (curLen + 1) / 2) % MOD;
+            res += (curLen * (curLen + 1) / 2);
             left = right;
         }
-        return (int)res;
+        return (int) (res % MOD);
     }
 }
