01ca7810a48b56f8d5e7834472958858c327bad9
chenwenzhuo <Chenwz1996@163.com>
2024-06-22 17:03:55 +0800

    493. 翻转对

--- LeetCode_JS/493_ReversePairs/Solution.js
+++ LeetCode_JS/493_ReversePairs/Solution.js
@@ -14,13 +14,15 @@ var reversePairs = function (nums) {
         const mid = (low + high) >> 1;
         mergeSort(low, mid);
         mergeSort(mid + 1, high);
+
         // 对于左半边的每个nums[i]，都去右半边寻找符合条件的元素
+        let end = mid + 1;
         for (let i = low; i <= mid; i++) {
-            for (let j = mid + 1; j <= high; j++) {
-                if (nums[i] > 2 * nums[j])
-                    cnt++;
-                else break;
-            }
+            // 增大end，直到不满足nums[i]>2*nums[end]
+            // 退出循环后，左闭右开区间[mid+1,end)内所有元素都满足要求
+            while (end <= high && nums[i] > 2 * nums[end])
+                end++;
+            cnt += (end - (mid + 1));
         }
 
         // 复制当前区间到cache中
